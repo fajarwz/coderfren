a6120ee166c44f1ee35f73a9e829469ae9309f63
Fajarwz <fajarwindhuzulfikar@gmail.com>
2024-02-28 14:37:14 +0700

    init

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Coderfren: Landing Page
+[Coderfren](https://coderfren.fajarwz.com/) is a discord server for software engineers around the world to ask questions and share knowledge so we can grow together.
+
+## Build Tailwind CSS
+Run the CLI tool to scan your template files for classes and build your CSS.
+```
+npm run watch:tw
+```
+
+## Run the App
+Run this application using tools like VS code extension [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+
+Hosted on [Netlify](https://www.netlify.com/).
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+// const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'badge-primary': '#1C3E74',
+        'badge-primary-light': '#EBF3FE',
+        'border': '#24416E',
+        'copyright': '#7587A3',
+        'dark': '#193766',
+        'dark-light': '#2B4772',
+        'muted': '#697B98',
+        'muted-light': '#FAFAFA',
+        'primary': '#3B82F6',
+        'primary-light': '#C6DBFF',
+        'secondary': '#E4EFFF',
+        'tertiary': '#1DA1F2',
+      },
+      width: {
+        '600px': '600px',
+        '537px': '537px',
+      },
+      height: {
+        '600px': '600px',
+        '364px': '364px',
+      },
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1140px',
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
